ee449ce1375c4d9b5732dcd34b5225e525f975f5
Eugene Lefter <eugen.lefter@gmail.com>
2024-04-25 18:23:55 -0400

    new updates

--- themes/react_example_theme/js/src/routes/CurrentUserProfile/fetchUserInfo.tsx
+++
@@ -1,5 +0,0 @@
-export const fetchUserInfo = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<Error | null>(null);
-  const [csrfToken, setCsrfToken] = useState<string | null>(null); 
-}
